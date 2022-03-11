e00efd5925c248b0459b7897f3ac55a33aa78e6d
Dennis O'Keeffe <hello@dennisokeeffe.com>
2022-03-11 18:56:35 +1000

    feat: add in subtract method

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -4,8 +4,13 @@ require 'contrived_math'
 RSpec.describe Math do
   describe '#add' do
     it 'adds two numbers' do
-      # binding.pry
       expect(ContrivedMath.add(1, 2)).to eq(3)
+    end
+  end
+
+  describe '#subtract' do
+    it 'subtracts two numbers' do
+      expect(ContrivedMath.subtract(2, 1)).to eq(1)
     end
   end
 end
